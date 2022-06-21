ec666995b8c53bb79b2c6b2f2b862f3976726df3
AlexanderHaan <haan.alex@outlook.de>
2022-06-21 17:04:14 +0200

    change font

--- init.el
+++ init.el
@@ -27,4 +27,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "SauceCodePro Nerd Font" :foundry "outline" :slant normal :weight normal :height 108 :width normal)))))
