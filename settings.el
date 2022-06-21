a573f58b12365ce16f0ca3927ffa4b211691c6d5
AlexanderHaan <haan.alex@outlook.de>
2022-06-21 18:15:00 +0200

    new configs

--- settings.el
+++ settings.el
@@ -3,7 +3,37 @@
 ;; do not create backup files
 (setq make-backup-files nil)
 
+;; Install Dracula theme
 (use-package dracula-theme
   :ensure t)
 
+;; Install Evil
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-C-u-scroll t)		; Enable Scroll up with C-u
+  :config
+  (evil-mode))
+
+;; Remap Escape key
+(use-package evil-escape
+  :ensure t
+  :init
+  (setq-default evil-escape-delay 0.2)
+  (setq-default evil-escape-key-sequence "jj")
+    (evil-escape-mode))
+
+;; Install Evil for Org-mode
+(use-package evil-org
+  :ensure t
+  :after org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-set-key-theme '(textobjects insert navigation additional shift todo heading))
+  (evil-org-agenda-set-keys))
+
+;; Set dracula theme
 (load-theme 'dracula t)
+;; disable tool bar
+(tool-bar-mode -1)
