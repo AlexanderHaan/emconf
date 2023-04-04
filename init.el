16e46f4871dd34b0b4e998da07c024884e4e6869
AlexanderHaan <haan.alex@outlook.de>
2023-04-04 09:45:01 +0200

    remove evil and add rust mode

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit evil-escape key-chord dracula-theme use-package org)))
+   '(tree-sitter-langs tree-sitter rust-mode magit use-package org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- settings.el
+++ settings.el
@@ -3,44 +3,24 @@
 ;; do not create backup files
 (setq make-backup-files nil)
 
-;; Install Dracula theme
-(use-package dracula-theme
-  :ensure t)
-
-;; Install Evil
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-C-u-scroll t)	; Enable Scroll up with C-u
-  (setq evil-auto-indent nil)	           ; Disable auto indent
-  :config
-  (evil-mode))
-
-;; Remap Escape key
-(use-package evil-escape
-  :ensure t
-  :init
-  (setq-default evil-escape-delay 0.2)
-  (setq-default evil-escape-key-sequence "jk")
-  (evil-escape-mode))
-
-;; Install Evil for Org-mode
-(use-package evil-org
-  :ensure t
-  :after org
-  :config  
-  (require 'evil-org-agenda)
-  (evil-org-set-key-theme '(textobjects insert navigation additional shift todo heading))
-  (add-hook 'org-mode-hook  (lambda ()
-    (evil-org-mode)))
-  (evil-org-agenda-set-keys))
-
 ;; Install magit
 (use-package magit
   :ensure t
   )
 
-;; Set dracula theme
-(load-theme 'dracula t)
+;; Install rust mode
+(use-package rust-mode
+  :ensure t
+  )
+
+;; Install tree sitter
+(use-package tree-sitter
+  :ensure t
+  )
+(use-package tree-sitter-langs
+  :ensure t
+  )
+(global-tree-sitter-mode)
+
 ;; disable tool bar
 (tool-bar-mode -1)
