c6a3872883c6e3db9b60a27b19ac17476d7dc8a6
AlexanderHaan <haan.alex@outlook.de>
2023-04-04 20:19:11 +0200

    added some org configs

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter rust-mode magit use-package org)))
+   '(org-bullets which-key tree-sitter-langs tree-sitter rust-mode magit use-package org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
